3047b94f24d2a804f9b049095577380bb44b6d03
Per Bergland <per@refapp.se>
2020-05-20 11:39:17 +0200

    writeDiagnosticMessage

--- meteor-typescript-compiler.ts
+++ meteor-typescript-compiler.ts
@@ -10,179 +10,202 @@ interface EmitResult {
 }
 
 export class MeteorTypescriptCompilerImpl {
-  private program: ts.EmitAndSemanticDiagnosticsBuilderProgram;
-  private diagnostics: ts.Diagnostic[];
+         private program: ts.EmitAndSemanticDiagnosticsBuilderProgram;
+         private diagnostics: ts.Diagnostic[];
 
-  error(msg: string, ...other: string[]) {
-    process.stderr.write(bold.red(msg) + reset(other.join(" ")) + "\n");
-  }
+         error(msg: string, ...other: string[]) {
+           process.stderr.write(bold.red(msg) + reset(other.join(" ")) + "\n");
+         }
 
-  info(msg: string) {
-    process.stdout.write(bold.green(msg) + dim(" ") + "\n");
-  }
+         info(msg: string) {
+           process.stdout.write(bold.green(msg) + dim(" ") + "\n");
+         }
 
-  writeDiagnostics(diagnostics: ts.Diagnostic[]) {
-    diagnostics.forEach((diagnostic) => {
-      if (diagnostic.file) {
-        let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-          diagnostic.start!
-        );
-        let message = ts.flattenDiagnosticMessageText(
-          diagnostic.messageText,
-          "\n"
-        );
-        this.info(
-          `${diagnostic.file.fileName} (${line + 1},${
-            character + 1
-          }): ${message}`
-        );
-      } else {
-        this.info(
-          `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
-        );
-      }
-    });
-  }
+         writeDiagnosticMessage(diagnostics: ts.Diagnostic, message:string) {
+          switch (diagnostics.category) {
+            case ts.DiagnosticCategory.Error:
+              return this.error(message);
+            case ts.DiagnosticCategory.Warning:
+            case ts.DiagnosticCategory.Suggestion:
+            case ts.DiagnosticCategory.Message:
+              return this.info(message);
+          }
+         }
 
-  startIncrementalCompilation() {
-    const configPath = ts.findConfigFile(
-      /*searchPath*/ "./",
-      ts.sys.fileExists,
-      "tsconfig.json"
-    );
-    if (!configPath) {
-      throw new Error("Could not find a valid 'tsconfig.json'.");
-    }
+         writeDiagnostics(diagnostics: ts.Diagnostic[]) {
+           diagnostics.forEach((diagnostic) => {
+             if (diagnostic.file) {
+               let {
+                 line,
+                 character,
+               } = diagnostic.file.getLineAndCharacterOfPosition(
+                 diagnostic.start!
+               );
+               let message = ts.flattenDiagnosticMessageText(
+                 diagnostic.messageText,
+                 "\n"
+               );
+               this.writeDiagnosticMessage(,
+                 diagnostic,`${diagnostic.file.fileName} (${line + 1},${
+                   character + 1
+                 }): ${message}`
+               );
+             } else {
+               this.writeDiagnosticMessage(diagnostic,
+                 `${ts.flattenDiagnosticMessageText(
+                   diagnostic.messageText,
+                   "\n"
+                 )}`
+               );
+             }
+           });
+         }
 
-    const buildInfoFile = ts.sys.resolvePath(
-      ".meteor/local/buildfile.tsbuildinfo"
-    );
-    const config = ts.getParsedCommandLineOfConfigFile(
-      configPath,
-      /*optionsToExtend*/ {
-        incremental: true,
-        tsBuildInfoFile: buildInfoFile,
-        noEmit: false,
-        importHelpers: true,
-        sourceMap: true,
-      },
-      /*host*/ {
-        ...ts.sys,
-        onUnRecoverableConfigFileDiagnostic: (d) =>
-          this.error(ts.flattenDiagnosticMessageText(d.messageText, "\n")),
-      }
-    );
-    if (!config) {
-      throw new Error("Could not parse 'tsconfig.json'.");
-    }
+         startIncrementalCompilation() {
+           const configPath = ts.findConfigFile(
+             /*searchPath*/ "./",
+             ts.sys.fileExists,
+             "tsconfig.json"
+           );
+           if (!configPath) {
+             throw new Error("Could not find a valid 'tsconfig.json'.");
+           }
 
-    this.program = ts.createIncrementalProgram({
-      rootNames: config.fileNames,
-      options: config.options,
-      configFileParsingDiagnostics: ts.getConfigFileParsingDiagnostics(config),
-      projectReferences: config.projectReferences,
-      // createProgram can be passed in here to choose strategy for incremental compiler just like when creating incremental watcher program.
-      // Default is ts.createSemanticDiagnosticsBuilderProgram
-    });
-    this.diagnostics = [
-      ...this.program.getConfigFileParsingDiagnostics(),
-      ...this.program.getSyntacticDiagnostics(),
-      ...this.program.getOptionsDiagnostics(),
-      ...this.program.getGlobalDiagnostics(),
-      ...this.program.getSemanticDiagnostics(), // Get the diagnostics before emit to cache them in the buildInfo file.
-    ];
+           const buildInfoFile = ts.sys.resolvePath(
+             ".meteor/local/buildfile.tsbuildinfo"
+           );
+           const config = ts.getParsedCommandLineOfConfigFile(
+             configPath,
+             /*optionsToExtend*/ {
+               incremental: true,
+               tsBuildInfoFile: buildInfoFile,
+               noEmit: false,
+               importHelpers: true,
+               sourceMap: true,
+             },
+             /*host*/ {
+               ...ts.sys,
+               onUnRecoverableConfigFileDiagnostic: (d) =>
+                 this.error(
+                   ts.flattenDiagnosticMessageText(d.messageText, "\n")
+                 ),
+             }
+           );
+           if (!config) {
+             throw new Error("Could not parse 'tsconfig.json'.");
+           }
 
-    /** Save out buildinfo */
-    this.program.emit(
-      undefined,
-      (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
-        if (fileName === buildInfoFile) {
-          this.info(`Writing ${buildInfoFile}`);
-          ts.sys.writeFile(fileName, data, writeByteOrderMark);
-        }
-      }
-    );
-    this.writeDiagnostics(this.diagnostics);
-  }
+           this.program = ts.createIncrementalProgram({
+             rootNames: config.fileNames,
+             options: config.options,
+             configFileParsingDiagnostics: ts.getConfigFileParsingDiagnostics(
+               config
+             ),
+             projectReferences: config.projectReferences,
+             // createProgram can be passed in here to choose strategy for incremental compiler just like when creating incremental watcher program.
+             // Default is ts.createSemanticDiagnosticsBuilderProgram
+           });
+           this.diagnostics = [
+             ...this.program.getConfigFileParsingDiagnostics(),
+             ...this.program.getSyntacticDiagnostics(),
+             ...this.program.getOptionsDiagnostics(),
+             ...this.program.getGlobalDiagnostics(),
+             ...this.program.getSemanticDiagnostics(), // Get the diagnostics before emit to cache them in the buildInfo file.
+           ];
 
-  prepareSourceMap(
-    sourceMapJson: string,
-    inputFile: MeteorCompiler.InputFile,
-    sourceFile: ts.SourceFile
-  ): Object {
-    const sourceMap: any = JSON.parse(sourceMapJson);
-    sourceMap.sourcesContent = [sourceFile.text];
-    sourceMap.sources = [inputFile.getPathInPackage()];
-    return sourceMap;
-  }
+           /** Save out buildinfo */
+           this.program.emit(
+             undefined,
+             (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
+               if (fileName === buildInfoFile) {
+                 this.info(`Writing ${buildInfoFile}`);
+                 ts.sys.writeFile(fileName, data, writeByteOrderMark);
+               }
+             }
+           );
+           this.writeDiagnostics(this.diagnostics);
+         }
 
-  emitForSource(
-    inputFile: MeteorCompiler.InputFile,
-    sourceFile: ts.SourceFile
-  ): EmitResult | undefined {
-    let result: EmitResult | undefined = undefined;
-    let sourceMap: Object | string | undefined = undefined;
+         prepareSourceMap(
+           sourceMapJson: string,
+           inputFile: MeteorCompiler.InputFile,
+           sourceFile: ts.SourceFile
+         ): Object {
+           const sourceMap: any = JSON.parse(sourceMapJson);
+           sourceMap.sourcesContent = [sourceFile.text];
+           sourceMap.sources = [inputFile.getPathInPackage()];
+           return sourceMap;
+         }
 
-    this.program.emit(
-      sourceFile,
-      (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
-        if (fileName.match(/\.map$/)) {
-          sourceMap = this.prepareSourceMap(data, inputFile, sourceFile);
-        } else {
-          result = { data, fileName, writeByteOrderMark, sourceFiles };
-        }
-      }
-    );
-    if (!result) {
-      return result;
-    }
-    return { ...result, sourceMap };
-  }
+         emitForSource(
+           inputFile: MeteorCompiler.InputFile,
+           sourceFile: ts.SourceFile
+         ): EmitResult | undefined {
+           let result: EmitResult | undefined = undefined;
+           let sourceMap: Object | string | undefined = undefined;
 
-  emitResultFor(inputFile: MeteorCompiler.InputFile) {
-    const inputFilePath = inputFile.getPathInPackage();
-    const sourceFile =
-      this.program.getSourceFile(inputFilePath) ||
-      this.program.getSourceFile(ts.sys.resolvePath(inputFilePath));
+           this.program.emit(
+             sourceFile,
+             (fileName, data, writeByteOrderMark, onError, sourceFiles) => {
+               if (fileName.match(/\.map$/)) {
+                 sourceMap = this.prepareSourceMap(data, inputFile, sourceFile);
+               } else {
+                 result = { data, fileName, writeByteOrderMark, sourceFiles };
+               }
+             }
+           );
+           if (!result) {
+             return result;
+           }
+           return { ...result, sourceMap };
+         }
 
-    if (!sourceFile) {
-      return;
-    }
-    try {
-      this.info(inputFile.getBasename());
-      const emitResult = this.emitForSource(inputFile, sourceFile);
-      if (!emitResult) {
-        this.error(`Nothing emitted for ${inputFilePath}`);
-        return;
-      }
-      const { data, fileName, sourceMap } = emitResult;
-      inputFile.addJavaScript({
-        data,
-        sourcePath: inputFilePath,
-        path: fileName,
-        sourceMap,
-      });
-    } catch (e) {
-      this.error(e.message);
-    }
-  }
+         emitResultFor(inputFile: MeteorCompiler.InputFile) {
+           const inputFilePath = inputFile.getPathInPackage();
+           const sourceFile =
+             this.program.getSourceFile(inputFilePath) ||
+             this.program.getSourceFile(ts.sys.resolvePath(inputFilePath));
 
-  processFilesForTarget(inputFiles: MeteorCompiler.InputFile[]) {
-    if (inputFiles.length > 0) {
-      this.info(`Typescript compilation for ${inputFiles[0].getArch()}`);
-    }
+           if (!sourceFile) {
+             return;
+           }
+           try {
+             this.info(inputFile.getBasename());
+             const emitResult = this.emitForSource(inputFile, sourceFile);
+             if (!emitResult) {
+               this.error(`Nothing emitted for ${inputFilePath}`);
+               return;
+             }
+             const { data, fileName, sourceMap } = emitResult;
+             inputFile.addJavaScript({
+               data,
+               sourcePath: inputFilePath,
+               path: fileName,
+               sourceMap,
+             });
+           } catch (e) {
+             this.error(e.message);
+           }
+         }
 
-    this.startIncrementalCompilation();
+         processFilesForTarget(inputFiles: MeteorCompiler.InputFile[]) {
+           if (inputFiles.length > 0) {
+             this.info(`Typescript compilation for ${inputFiles[0].getArch()}`);
+           }
 
-    const isCompilableFile = (f: MeteorCompiler.InputFile) => {
-      const fileName = f.getBasename();
-      return !fileName.match(/.d.ts$/) && !fileName.match(/^tsconfig.json$/i);
-    };
+           this.startIncrementalCompilation();
 
-    for (const inputFile of inputFiles.filter(isCompilableFile)) {
-      this.emitResultFor(inputFile);
-    }
-  }
-}
+           const isCompilableFile = (f: MeteorCompiler.InputFile) => {
+             const fileName = f.getBasename();
+             return (
+               !fileName.match(/.d.ts$/) && !fileName.match(/^tsconfig.json$/i)
+             );
+           };
+
+           for (const inputFile of inputFiles.filter(isCompilableFile)) {
+             this.emitResultFor(inputFile);
+           }
+         }
+       }
 
 MeteorTypescriptCompiler = MeteorTypescriptCompilerImpl;
